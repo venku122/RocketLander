360558fc5e8e346d8def0dda1eec206e5962bc60
T.J. Tarazevits <tarazevits@gmail.com>
2016-03-23 11:45:58 -0700

    Setup basic HTML and App design pattern
    -Created Rocket Lander html page with canvas element
    -created main.js which will be the main game loop
    --added ctx and canvas element properties
    -created loader.js to manage loading and running of scripts

---
+++ js/loader.js
@@ -0,0 +1,19 @@
+/*
+loader.js
+variable 'app' is in global scope - i.e. a property of window.
+app is our single global object literal - all other functions and properties of 
+the game will be properties of app.
+*/
+"use strict";
+
+// if app exists use the existing copy
+// else create a new empty object literal
+var app = app || {};
+
+
+window.onload = function(){
+	console.log("window.onload called");
+
+	app.main.init();
+	
+};
---
+++ js/main.js
@@ -0,0 +1,33 @@
+// main.js
+// Dependencies: 
+// Description: singleton object
+// This object will be our main "controller" class and will contain references
+// to most of the other objects in the game.
+
+"use strict";
+
+// if app exists use the existing copy
+// else create a new object literal
+var app = app || {};
+
+/*
+ .main is an object literal that is a property of the app global
+ This object literal has its own properties and methods (functions)
+ 
+ */
+ app.main = {
+	 
+	 
+	 //Properties
+	canvas: undefined,
+    ctx: undefined,
+	
+	init : function(){
+		console.log("app.main.init() called");
+		// initialize properties
+		this.canvas = document.querySelector('canvas');
+		this.canvas.width = this.WIDTH;
+		this.canvas.height = this.HEIGHT;
+		this.ctx = this.canvas.getContext('2d');
+	}
+ }
