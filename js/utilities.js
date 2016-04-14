ccbabd4811d1e6a097cb7240df4f55a657031ff6
Aidan McInerny <Aidan McInerny>
2016-04-14 03:20:26 -0400

    Finalizing UI.

--- js/utilities.js
+++ js/utilities.js
@@ -5,7 +5,7 @@
 // returns mouse position in local coordinate system of element
 function getMouse(e){
 	var mouse = {} // make an object
-	mouse.x = e.pageX - e.target.offsetLeft - 150;
+	mouse.x = e.pageX - e.target.offsetLeft;
 	mouse.y = e.pageY - e.target.offsetTop;
 	return mouse;
 }
